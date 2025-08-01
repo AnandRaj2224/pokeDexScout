f1120851bd74722f7b923f109a0929873da040c7
Anand <anandraj6783@gmail.com>
2025-08-01 19:38:53 +0530

    made background and imported the images to use

---
+++ src/components/Background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Pokeball1 from "../../public/assets/pokeball.png";
+import Pokeball2 from "../../public/assets/pokeball2.png";
+
+function Background() {
+  return (
+    <div className="background">
+      <img src={Pokeball1} alt="Pokeball" className="pokeball pokeball1" />
+      <img src={Pokeball2} alt="Pokeball" className="pokeball pokeball2" />
+      <img src={Pokeball1} alt="Pokeball" className="pokeball pokeball3" />
+      <img src={Pokeball2} alt="Pokeball" className="pokeball pokeball4" />
+      <img src={Pokeball1} alt="Pokeball" className="pokeball pokeball5" />
+      <img src={Pokeball2} alt="Pokeball" className="pokeball pokeball6" />
+    </div>
+  );
+}
+
+export default Background;
