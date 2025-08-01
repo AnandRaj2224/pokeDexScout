36692753f49f7f4206461ae1ed6b13a3e3b08af7
Anand <anandraj6783@gmail.com>
2025-08-01 19:56:59 +0530

    added meu icon and pokeball icon

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import pokeballIcon from "../../public/assets/pokeball-icon.png";
 
 function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+  return( <nav>
+    <div className="block">
+      <img src={pokeballIcon} alt="pokeball icon" />
+    </div>
+    <div className="data"></div>
+    <div className="block">
+      <GiHamburgerMenu/>
+    </div>
+  </nav>
+  );
 }
 
 export default NavBar
