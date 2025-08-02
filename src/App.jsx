d784cc10faa5112f56e1888c5db4ca5fe13f537c
Anand <anandraj6783@gmail.com>
2025-08-02 20:30:52 +0530

    change assets location to src

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import Background from './components/Background';
 import NavBar from './sections/NavBar'
 import Wrapper from './sections/Wrapper'
 import Footer from './sections/Footer'
+import {BrowserRouter,Route,Routes,Navigate} from 'react-router-dom'
 
 
 function App() {
   return <div className='main-container'>
     <Background />
+    <BrowserRouter>
     <div className='app'>
       <NavBar />
+      <Routes>
+        <Route element ={<Search />} path='/search'/>
+        <Route element ={<MyList />} path='/list'/>
+        <Route element ={<About />} path='/about'/>
+        <Route element ={<Compare />} path='/compare'/>
+        <Route element ={<Pokemon />} path='/pokemon/:id'/>
+        <Route element ={<Navigate to="/pokemon/1" />} path='*'/>
+
+
+      </Routes>
       <Wrapper />
       <Footer />
     </div>
+    </BrowserRouter>
   </div>
 }
 
--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import pokeballIcon from "../../public/assets/pokeball-icon.png";
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const navigateRoutes = [
+  { name: 'Search', route: '/search' },
+  { name: 'Pokemon', route: '/pokemon' },
+  { name: 'My list', route: '/list' },
+  { name: 'Compare', route: '/compare' },
+  { name: 'About', route: '/about' },
+];
   return( <nav>
     <div className="block">
       <img src={pokeballIcon} alt="pokeball icon" />
     </div>
-    <div className="data"></div>
+    <div className="data">
+      <ul>
+        {
+          navigateRoutes.map(({name,route},index) =>{
+            return <Link to={route} key={index}>
+              <li>
+                {name}
+              </li>
+            </Link>
+          }) 
+        }
+      </ul>
+    </div>
     <div className="block">
       <GiHamburgerMenu/>
     </div>
