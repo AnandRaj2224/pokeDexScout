3c19eb3a9523dcfbfc1e6b0b7472ea96aeb9528c
Anand <anandraj6783@gmail.com>
2025-08-05 10:56:32 +0530

    defined allpokemon initial state

--- src/App/slices/PokemonSlice.js
+++ src/App/slices/PokemonSlice.js
@@ -1,13 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getInitialPokemonData } from '../reducers/getInitialPokemonData';
 
 const initialState = {
-  // your pokemon state here
+  allPokemon : undefined,
 };
 
 const pokemonSlice = createSlice({
   name: 'pokemon',
   initialState,
   reducers: {},
+  extraReducers : (builder) => {
+    builder.addCase(getInitialPokemonData.fulfilled,(state,action) => {
+      state.allPokemon = action.payload
+    });
+  }
 });
 
 export default pokemonSlice.reducer;
