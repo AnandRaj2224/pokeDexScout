c31cfff8184aa1ffa315afab530ff72c34074993
Anand <anandraj6783@gmail.com>
2025-08-07 10:54:06 +0530

    added pokemon types to the pokemon card

--- src/components/PokemonCardGrid.jsx
+++ src/components/PokemonCardGrid.jsx
@@ -4,16 +4,36 @@ function PokemonCardGrid({ Pokemons }) {
   return (
     <div className="pokemon-card-grid-container">
       <div className="pokemon-card-grid">
-        {Pokemons && Pokemons.length > 0 && Pokemons.map((data) => {
-          return (
-            <div className="pokemon-card" key={data.id}>
-              <div className="pokemon-card-list"></div>
-              <div className="pokemon-card-compare"></div>
-              <h3 className="pokemon-card-title">{data.name}</h3>
-              <img src={data.image} alt={data.name} className="pokemon-card-image" />
+        {Pokemons && Pokemons.length > 0 && Pokemons.map((data) => (
+          <div className="pokemon-card" key={data.id}>
+            <div className="pokemon-card-list"></div>
+            <div className="pokemon-card-compare"></div>
+            <h3 className="pokemon-card-title">{data.name}</h3>
+            <img
+              src={data.image}
+              alt={data.name}
+              className="pokemon-card-image"
+              loading="lazy"
+            />
+            <div className="pokemon-card-types">
+              {data.types.map((type, index) => {
+                const keys = Object.keys(type);
+                return (
+                  <div className="pokemon-card-types-type" key={index}>
+                    <img
+                      src={type[keys[0]].image}
+                      alt="pokemon type"
+                      loading="lazy"
+                    />
+                    <h6 className="pokemon-card-types-type-text">
+                      {keys[0]}
+                    </h6>
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
