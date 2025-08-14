d8eb8810835e31f97d56bc8e0209e83f849b88b5
Anand <anandraj6783@gmail.com>
2025-08-14 21:42:40 +0530

    index file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import store from './app/store.js'
-import { Provider } from 'react-redux'
- 
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />
-    </Provider>
-  )
+  </Provider>
+);
