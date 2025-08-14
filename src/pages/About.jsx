c1fcd7075ed07d24c1b86c5a2b66bee9f89b5e46
Anand <anandraj6783@gmail.com>
2025-08-14 21:33:26 +0530

    about me and links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import Wrapper from '../sections/Wrapper';
+import React from "react";
+import Wrapper from "../sections/Wrapper";
+import avatarImage from "../assets/kishan.jpeg";
+import {  FaGithub, FaLinkedin } from "react-icons/fa";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="profile">
+      <img src={avatarImage} alt="" className="profile-image" />
+      <h1 className="profile-text">Hi I am Anand Raj</h1>
+      <div className="profile-links">
+        <a href="https://github.com/AnandRaj2224">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/AnandRaj/">
+          <FaLinkedin />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Wrapper(About);
