88b826020306db4b592ef38a4e15f0e8d4bb7db7
Anand <anandraj6783@gmail.com>
2025-08-14 21:46:40 +0530

    load pokemons from poke api

--- src/App/reducers/getPokemonData.js
+++ src/App/reducers/getPokemonData.js
@@ -1,49 +1,35 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { defaultImages, images } from "../../utils/pokemonImages";
-import { pokemonTypes } from "../../utils/pokemonTypes";
+import { defaultImages, images, pokemonTypes } from "../../utils";
 
-export const getPokemonData = createAsyncThunk(
-  "pokemon/getPokemonData",
+export const getPokemonsData = createAsyncThunk(
+  "pokemon/randomPokemon",
   async (pokemons) => {
     try {
       const pokemonsData = [];
       for await (const pokemon of pokemons) {
-        // Fetch detailed data for each Pokémon
         const { data } = await axios.get(pokemon.url);
+        const types = data.types.map(({ type: { name } }) => ({
+          [name]: pokemonTypes[name],
+        }));
 
-        // Map over the types from the API response
-        const types = data.types.map(({ type: { name } }) => {
-          // For each type name (e.g., "grass"), create an object
-          return {
-            // 1. Add the name of the type
-            name: name,
-            // 2. Spread all properties (image, strength, etc.) from our pokemonTypes object
-            ...pokemonTypes[name],
-          };
-        });
-
-        // Handle Pokémon image
         let image = images[data.id];
         if (!image) {
           image = defaultImages[data.id];
         }
 
-        // Push the final, structured Pokémon object to our array
         if (image) {
           pokemonsData.push({
-            name: data.name,
+            name: pokemon.name,
             id: data.id,
             image,
-            types, // This is our new, easy-to-use types array
+            types,
           });
         }
       }
       return pokemonsData;
     } catch (err) {
-      console.error("Failed to fetch Pokémon data:", err);
-      // Return an empty array or handle the error as needed
-      return []; 
+      console.error(err);
     }
   }
 );
