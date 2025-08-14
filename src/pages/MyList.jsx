cf9eb0155eadd4be2aeb491ac057f10690eff35a
Anand <anandraj6783@gmail.com>
2025-08-14 21:33:43 +0530

    my list page done

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import Wrapper from '../sections/Wrapper';
+import React, { useEffect } from "react";
+import Wrapper from "../sections/Wrapper";
+import Login from "../components/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserPokemons } from "../app/reducers/getUserPokemons";
+import PokemonCardGrid from "../components/PokemonCardGrid";
 
 function MyList() {
+  const { userInfo } = useSelector(({ app }) => app);
+  const { userPokemons } = useSelector(({ pokemon }) => pokemon);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getUserPokemons());
+  }, [userInfo, dispatch]);
+
   return (
-    <div>MyList</div>
-  )
+    <div className="list">
+      {userInfo ? <PokemonCardGrid pokemons={userPokemons} /> : <Login />}
+    </div>
+  );
 }
 
 export default Wrapper(MyList);
