e6ee6914644bb39a40b71b710fa58159aa7afad7
Anand <anandraj6783@gmail.com>
2025-08-14 21:26:07 +0530

    info for pokemon

---
+++ src/components/Info.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect } from "react";
+import { pokemonTypes } from "../utils";
+import { useDispatch } from "react-redux";
+import { addPokemonToList } from "../app/reducers/addPokemonToList";
+import { setPokemonTab } from "../app/slices/AppSlice";
+import { pokemonTabs } from "../utils/constants";
+
+export default function Info({ data }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const progressBars = document.querySelectorAll("progress");
+    progressBars.forEach((progressBar) => {
+      progressBar.style.width = "10rem";
+    });
+  }, []);
+
+  const createStatsArray = (types, statType) => {
+    const statsSet = new Set();
+    if (types) {
+      types.forEach((type) => {
+        // @ts-ignore
+        pokemonTypes[type][statType].forEach((stat) => {
+          if (!statsSet.has(stat)) {
+            statsSet.add(stat[0].toUpperCase() + stat.slice(1));
+          }
+        });
+      });
+    }
+    return Array.from(statsSet);
+  };
+
+  return (
+    <>
+      <div className="details">
+        <h1 className="name">{data?.name}</h1>
+        <h3>Type: {data?.types.join(" - ")}</h3>
+        <h3>Evolution: {data?.evolutionLevel}</h3>
+        <button onClick={() => dispatch(setPokemonTab(pokemonTabs.evolution))}>
+          See next evolution
+        </button>
+      </div>
+      <div className="stats">
+        <ul>
+          {data?.stats.map((stat) => {
+            return (
+              <li key={stat.name}>
+                {stat.name}: {stat.value}
+                <progress max={100} value={stat.value} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="battle-stats">
+        {
+          <ul>
+            <li>
+              <span>Strengths:</span>
+              <span>
+                {createStatsArray(data?.types, "strength").join(", ")}
+              </span>
+            </li>
+            <li>
+              <span>Weakness:</span>
+              <span>
+                {createStatsArray(data?.types, "weakness").join(", ")}
+              </span>
+            </li>
+            <li>
+              <span>Resistant:</span>
+              <span>
+                {createStatsArray(data?.types, "resistance").join(", ")}
+              </span>
+            </li>
+            <li>
+              <span>Vulnerable:</span>
+              <span>
+                {createStatsArray(data?.types, "vulnerable").join(", ")}
+              </span>
+            </li>
+          </ul>
+        }
+        <button
+          onClick={() => dispatch(addPokemonToList(data))}
+          className="add-pokemon"
+        >
+          Add Pokemon
+        </button>
+      </div>
+    </>
+  );
+}
