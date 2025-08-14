1b4e0b1c2a79c7268a02d24b387dec6f53cf50be
Anand <anandraj6783@gmail.com>
2025-08-14 21:26:29 +0530

    completed

--- src/components/PokemonCardGrid.jsx
+++ src/components/PokemonCardGrid.jsx
@@ -1,71 +1,90 @@
-import React from 'react';
-import { IoGitCompareSharp } from 'react-icons/io5';
-import { FaTrash, FaPlus } from 'react-icons/fa';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../app/hooks';
-import { addToCompare } from '../app/slices/PokemonSlice';
-import { setToast } from '../app/slices/AppSlice';
+import React from "react";
+import { IoGitCompare } from "react-icons/io5";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import { addToCompare, setCurrentPokemon } from "../app/slices/PokemonSlice";
+import { useDispatch } from "react-redux";
+import { removePokemonFromUserList } from "../app/reducers/removePokemonFromUserList";
+import { useLocation, useNavigate } from "react-router-dom";
+import { setPokemonTab, setToast } from "../app/slices/AppSlice";
+import { addPokemonToList } from "../app/reducers/addPokemonToList";
+import { pokemonTabs } from "../utils/constants";
 
-function PokemonCardGrid({ Pokemons }) {
-  const location = useLocation();
+function PokemonCardGrid({ pokemons }) {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-
+  const location = useLocation();
   return (
     <div className="pokemon-card-grid-container">
       <div className="pokemon-card-grid">
-        {Pokemons &&
-          Pokemons.length > 0 &&
-          Pokemons.map((data) => (
-            <div className="pokemon-card" key={data.id}>
-              <div className="pokemon-card-list">
-                {location.pathname.includes('/pokemon') ||
-                location.pathname.includes('/search') ? (
-                  <FaPlus className="plus" />
-                ) : (
-                  <FaTrash className="trash" />
-                )}
-              </div>
-              <div className="pokemon-card-compare">
-                <IoGitCompareSharp
+        {pokemons &&
+          pokemons.length > 0 &&
+          pokemons?.map((data) => {
+            return (
+              <div key={data.id} className="pokemon-card">
+                <div className="pokemon-card-list">
+                  {location.pathname.includes("/pokemon") ||
+                  location.pathname.includes("/search") ? (
+                    <FaPlus
+                      className="plus"
+                      onClick={() => dispatch(addPokemonToList(data))}
+                    />
+                  ) : (
+                    <FaTrash
+                      className="trash"
+                      onClick={async () => {
+                        await dispatch(
+                          removePokemonFromUserList({ id: data.firebaseId })
+                        );
+                        dispatch(setToast("Pokemon Removed Successfully."));
+                      }}
+                    />
+                  )}
+                </div>
+                <div className="pokemon-card-compare">
+                  <IoGitCompare
+                    onClick={() => {
+                      dispatch(addToCompare(data));
+                      dispatch(
+                        setToast(
+                          `${data.name} has been added to compare queue.`
+                        )
+                      );
+                    }}
+                  />
+                </div>
+                <h3 className="pokemon-card-title">{data.name}</h3>
+                <img
+                  src={data.image}
+                  alt=""
+                  className="pokemon-card-image"
+                  loading="lazy"
                   onClick={() => {
-                    dispatch(addToCompare(data));
-                    dispatch(
-                      setToast(`${data.name} has been added to Compare Queue.`)
-                    );
+                    dispatch(setPokemonTab(pokemonTabs.description));
+                    dispatch(setCurrentPokemon(undefined));
+                    navigate(`/pokemon/${data.id}`);
                   }}
                 />
+                <div className="pokemon-card-types">
+                  {data.types.map((type, index) => {
+                    const keys = Object.keys(type);
+                    return (
+                      <div className="pokemon-card-types-type" key={index}>
+                        <img
+                          src={type[keys[0]].image}
+                          alt="pokemon type"
+                          className="pokemon-card-types-type-image"
+                          loading="lazy"
+                        />
+                        <h6 className="pokemon-card-types-type-text">
+                          {keys[0]}
+                        </h6>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-              <h3 className="pokemon-card-title">{data.name}</h3>
-              <img
-                src={data.image}
-                alt={data.name}
-                className="pokemon-card-image"
-                loading="lazy"
-                onClick={() => navigate(`/pokemon/${data.id}`)}
-              />
-              <div className="pokemon-card-types">
-                {data.types.map((type, index) => {
-                  const keys = Object.keys(type);
-                  return (
-                    <div
-                      className="pokemon-card-types-type"
-                      key={`${data.id}-${index}`}
-                    >
-                      <img
-                        src={type[keys[0]].image}
-                        alt="pokemon type"
-                        loading="lazy"
-                      />
-                      <h6 className="pokemon-card-types-type-text">
-                        {keys[0]}
-                      </h6>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
