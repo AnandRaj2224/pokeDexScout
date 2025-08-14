e74a4685904ed539e9ffb2f8e75d140da36f10f7
Anand <anandraj6783@gmail.com>
2025-08-14 21:51:22 +0530

    added reducers

--- src/App/slices/AppSlice.js
+++ src/App/slices/AppSlice.js
@@ -1,21 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { pokemonTabs } from "../../utils/constants";
 
 const initialState = {
+  isLoading: true,
+  userInfo: undefined,
   toasts: [],
+  currentPokemonTab: pokemonTabs.description,
 };
 
-const appSlice = createSlice({
-  name: 'app',
+export const AppSlice = createSlice({
+  name: "app",
   initialState,
   reducers: {
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setUserStatus: (state, action) => {
+      state.userInfo = action.payload;
+    },
     setToast: (state, action) => {
-      state.toasts.push(action.payload);
+      const toasts = [...state.toasts];
+      toasts.push(action.payload);
+      state.toasts = toasts;
     },
     clearToasts: (state) => {
       state.toasts = [];
     },
+    setPokemonTab: (state, action) => {
+      state.currentPokemonTab = action.payload;
+    },
   },
 });
 
-export const { setToast, clearToasts } = appSlice.actions;
-export default appSlice.reducer;
+export const {
+  setLoading,
+  setUserStatus,
+  setToast,
+  clearToasts,
+  setPokemonTab,
+} = AppSlice.actions;
